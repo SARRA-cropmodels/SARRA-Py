08c59af7b327c11b75245a4dd12f096da78b6985
Jérémy Lavarenne (@codename5281) <jeremy.lavarenne@yahoo.com>
2022-06-24 11:44:50 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import os
+
+
+def parse_requirements(file):
+    return sorted(set(
+        line.partition('#')[0].strip()
+        for line in open(os.path.join(os.path.dirname(__file__), file))
+    ) - set(''))
+
+
+setup(
+	name='SARRA-Py',
+	python_requires='>=3.9.6',
+	description='SARRA-Py',
+	version = '0.0.1',
+	author='Jérémy LAVARENNE (@codename5281)',
+	packages=find_packages(where='src'),
+	install_requires=parse_requirements('requirements.txt'),
+	package_dir={'': 'src'}
+	)
