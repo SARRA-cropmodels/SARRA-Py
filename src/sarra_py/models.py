6ced150dae671226248aab525843a69c85e91f9a
Jérémy Lavarenne <jeremy.lavarenne@cirad.fr>
2023-08-30 17:04:55 +0200

    updated compute_irrigation_state function name

--- src/sarra_py/models.py
+++ src/sarra_py/models.py
@@ -27,11 +27,11 @@ def run_model(paramVariete, paramITK, paramTypeSol, data, duration):
         data = EvalPhenoSarrahV3(j, data, paramITK, paramVariete)
 
         # sum of thermal sime is being computed from the day the crop is sown, including the day of sowing
-        data = calculate_sum_of_thermal_time(j, data, paramVariete)
+        data = calculate_sum_of_thermal_time(j, data)
 
-        # water balance
-        # evalIrrigPhase sp&cifique de l'irrigation automatique, on peut presque le conditionner au irrigAuto==True
-        data = EvalIrrigPhase(j, data, paramITK)
+        ### water balance
+        data = compute_irrigation_state(j, data, paramITK)
+
         # sums rainfall and irrigation history
         data = calculate_total_water_availability(j, data)
         # can be conditioned to the presence of mulch
@@ -141,11 +141,10 @@ def run_waterbalance_model(paramVariete, paramITK, paramTypeSol, data, duration)
         data = EvalPhenoSarrahV3(j, data, paramITK, paramVariete)
 
         # sum of thermal sime is being computed from the day the crop is sown, including the day of sowing
-        data = calculate_sum_of_thermal_time(j, data, paramVariete)
+        data = calculate_sum_of_thermal_time(j, data)
 
         # water balance
-        # evalIrrigPhase sp&cifique de l'irrigation automatique, on peut presque le conditionner au irrigAuto==True
-        data = EvalIrrigPhase(j, data, paramITK)
+        data = compute_irrigation_state(j, data, paramITK)
         # sums rainfall and irrigation history
         data = calculate_total_water_availability(j, data)
         # can be conditioned to the presence of mulch
@@ -182,49 +181,6 @@ def run_waterbalance_model(paramVariete, paramITK, paramTypeSol, data, duration)
         data = estimate_ltr(j, data, paramVariete)
         data = estimate_KAssim(j, data, paramVariete)
         data = estimate_conv(j,data,paramVariete)
-
-        # # adjusting for sowing densité, in
-        # data = adjust_for_sowing_density(j, data, paramVariete, direction = "in") # ***bilancarbonsarra*** # trad OK
-        
-        # data = update_assimPot(j, data, paramVariete, paramITK)
-        # data = update_assim(j, data)
-
-        # data = calculate_maintainance_respiration(j, data, paramVariete)
-        # data = update_total_biomass(j, data, paramVariete, paramITK)
-
-
-        # data = update_total_biomass_stade_ip(j, data)
-        # data = update_total_biomass_at_flowering_stage(j, data)
-        # data = update_potential_yield(j, data, paramVariete)
-        # data = update_potential_yield_delta(j, data, paramVariete)
-
-        # data = update_aboveground_biomass(j, data, paramVariete)
-
-        # data = estimate_reallocation(j, data, paramVariete)
-
-        # data = update_root_biomass(j, data)
-        # data = EvalFeuilleTigeSarrahV4(j, data, paramVariete)
-        # data = update_vegetative_biomass(j, data)
-
-        # data = calculate_canopy_specific_leaf_area(j, data, paramVariete)
-        # data = calculate_leaf_area_index(j, data)
-        
-        # data = update_yield_during_filling_phase(j, data) 
-        
-        # #phenologie
-        # data = update_photoperiodism(j, data, paramVariete)
-        
-        # # # bilan carbone
-        # data = MortaliteSarraV3(j, data, paramITK, paramVariete)
-        
-        
-        # data = adjust_for_sowing_density(j, data, paramVariete, direction="out")
-        
-        
-        # # data = BiomMcUBTSV3(j, data, paramITK) # ***bilancarbonsarra***, exmodules 2
-        # # data = MAJBiomMcSV3(data) # ***bilancarbonsarra***, exmodules 2
-
-        # data = estimate_critical_nitrogen_concentration(j, data)
 
 
     return data
